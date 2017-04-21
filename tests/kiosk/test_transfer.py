990f1483d8b4ac218b3628c632eaffa6f6923126
Chris Churas <churas@gmail.com>
2017-04-21 14:52:12 -0700

    initial revision

---
+++ tests/kiosk/test_transfer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_transfer
+----------------------------------
+
+Tests for `transfer` module.
+"""
+
+import shutil
+import tempfile
+import sys
+import unittest
+import os
+
+class TestTransfer(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_foo(self):
+        self.assertEqual(2, 2)
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
