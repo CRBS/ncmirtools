a901adb616070a89b945587096a53988d2ae3779
Chris Churas <churas@gmail.com>
2017-04-24 20:48:19 -0500

    refactored SftpTransfer code and moved definition of option keywords into class

--- ncmirtools/kiosk/transfer.py
+++ ncmirtools/kiosk/transfer.py
@@ -6,6 +6,9 @@ import os
 import time
 import logging
 import paramiko
+from configparser import NoOptionError
+from configparser import NoSectionError
+
 from ncmirtools.config import NcmirToolsConfig
 
 
@@ -46,42 +49,104 @@ class Transfer(object):
 
 class SftpTransfer(Transfer):
     """Transfers file to remote server via SFTP
+       defined by a `configparser.ConfigParser` passed into
+       the constructor with the following values set:
+
+       [`SECTION`]
+       `HOST` = <remote host>*
+       `USER` = <user name ie bob>
+       `PORT` = <port to use ie 22>
+       `KEY` = <private key if used>
+       `DEST_DIR` = <destination directory on remote host>*
+
+       NOTE: lines above with * are required
     """
+    SECTION = 'sftptransfer'
+    HOST = 'host'
+    USER = 'username'
+    PORT = 'port'
+    KEY = 'private_key'
+    DEST_DIR = 'destination_dir'
+    CON_TIMEOUT = 'connect_timeout'
+
+    DEFAULT_PORT = 22
+    DEFAULT_CON_TIMEOUT = 60
+
     def __init__(self, config):
+        """Constructor
+        :param config: configparser.ConfigParser object set with
+                       with values set as described in constructor
+                       documentation
+        """
         super(SftpTransfer, self).__init__(config)
         self._altssh = None
         self._ssh = None
         self._sftp = None
 
-    def _get_private_key(self):
+    def _get_value_from_config(self, section, option):
+        """Gets value from configuration
+        :param section: section containing option keyword
+        :param optionkey: option keyword
+        :returns value for keyword or None
+        """
+        if self._config is None:
+            logger.error('Configuration for SftpTransfer is None')
+            return None
+        try:
+            return self._config.get(section, option)
+        except NoSectionError:
+            logger.info('No section named: ' + str(section))
+            return None
+        except NoOptionError:
+            logger.info('No option named: ' + str(option))
+            return None
+
+    def _getprivatekey(self):
         """Gets private key from config
         """
-        pkey_file = self._config.get(NcmirToolsConfig.DATASERVER_SSH_SECTION,
-                                     NcmirToolsConfig.DATASERVER_SSH_KIOSKKEY)
-        return paramiko.RSAKey.from_private_key_file(pkey_file)
+        pkey_file = self._get_value_from_config(SftpTransfer.SECTION,
+                                                SftpTransfer.KEY)
+        if pkey_file is not None:
+            logger.debug('Key file => ' + str(pkey_file))
+            return paramiko.RSAKey.from_private_key_file(pkey_file)
+        return None
 
-    def _get_kioskserver(self):
-        return self._config.get(NcmirToolsConfig.DATASERVER_SECTION,
-                                NcmirToolsConfig.DATASERVER_KIOSKSERVER)
+    def _gethost(self):
+        return self._get_value_from_config(SftpTransfer.SECTION,
+                                           SftpTransfer.HOST)
 
-    def _get_kioskusername(self):
-        return self._config.get(NcmirToolsConfig.DATASERVER_SSH_SECTION,
-                                NcmirToolsConfig.DATASERVER_SSH_KIOSKUSER)
+    def _getusername(self):
+        return self._get_value_from_config(SftpTransfer.SECTION,
+                                           SftpTransfer.USER)
 
     def _get_missing_host_key_policy(self):
         return paramiko.AutoAddPolicy()
 
-    def _get_kioskport(self):
-        return 22
+    def _getport(self):
+        val = self._get_value_from_config(SftpTransfer.SECTION,
+                                           SftpTransfer.PORT)
+        if val is None:
+            return SftpTransfer.DEFAULT_PORT
 
-    def _get_kioskconnecttimeout(self):
-        return 60
+        logger.debug('Using user set port of: ' + str(val))
+        return int(val)
 
-    def _get_kioskdir(self):
-        return self._config.get(NcmirToolsConfig.DATASERVER_SECTION,
-                                NcmirToolsConfig.DATASERVER_KIOSKDIR)
+    def _getconnecttimeout(self):
+        val = self._get_value_from_config(SftpTransfer.SECTION,
+                                          SftpTransfer.CON_TIMEOUT)
+        if val is None:
+            return SftpTransfer.DEFAULT_CON_TIMEOUT
+        return int(val)
+
+    def _getdestdir(self):
+        return self._get_value_from_config(SftpTransfer.SECTION,
+                                           SftpTransfer.DEST_DIR)
 
     def set_alternate_connection(self, altssh):
+        """Sets alternate ssh connection
+        :param altssh: Object that is paramiko.SSHClient or one that
+                    implements put,close,set_missing_host_key_policy
+        """
         self._altssh = altssh
 
     def connect(self):
@@ -90,27 +155,32 @@ class SftpTransfer(Transfer):
             logger.info('Alternate ssh connection set, using instead')
             self._ssh = self._altssh
             return
-        logger.debug('Connecting via ssh to ' + str(self._get_kioskserver()))
+        logger.info('Connecting via ssh to ' + str(self._gethost()))
         self._ssh = paramiko.SSHClient()
         self._ssh.set_missing_host_key_policy(self._get_missing_host_key_policy())
-        self._ssh.connect(hostname=self._get_kioskserver(),
-                          username=self._get_kioskusername(),
-                          pkey=self._get_private_key(),
-                          port=self._get_kioskport(),
-                          timeout=self._get_kioskconnecttimeout()
-                          )
+        self._ssh.connect(hostname=self._gethost(),
+                          username=self._getusername(),
+                          pkey=self._getprivatekey(),
+                          port=self._getport(),
+                          timeout=self._getconnecttimeout())
         logger.debug('Connection completed')
 
     def disconnect(self):
         """Disconnects
         """
         logger.debug('Disconnecting from ssh server ' +
-                     str(self._get_kioskserver()))
+                     str(self._gethost()))
         if self._sftp is not None:
-            self._sftp.close()
+            try:
+                self._sftp.close()
+            except Exception:
+                logger.error('Caught exception closing sftp connection')
             self._sftp = None
         if self._ssh is not None:
-            self._ssh.close()
+            try:
+                self._ssh.close()
+            except Exception:
+                logger.error('Caught exception closing ssh connection')
             self._ssh = None
 
     def transfer_file(self, filepath):
@@ -120,9 +190,9 @@ class SftpTransfer(Transfer):
             logger.debug('Creating SFTP connection')
             self._sftp = self._ssh.open_sftp()
 
-        kiosk_dir = self._get_kioskdir()
+        dest_dir = self._getdestdir()
 
-        dest_file = os.path.join(kiosk_dir, os.path.basename(filepath))
+        dest_file = os.path.join(dest_dir, os.path.basename(filepath))
         logger.info('Uploading ' + str(filepath) + ' to ' + dest_file)
 
         transfer_err_msg = None
--- tests/kiosk/test_transfer.py
+++ tests/kiosk/test_transfer.py
@@ -13,8 +13,44 @@ import tempfile
 import sys
 import unittest
 import os
+import configparser
+from configparser import NoOptionError
+from configparser import NoSectionError
+
+from ncmirtools.config import NcmirToolsConfig
+from ncmirtools.kiosk.transfer import Transfer
+from ncmirtools.kiosk.transfer import SftpTransfer
 
 class TestTransfer(unittest.TestCase):
+
+    def _get_dummy_private_key(self):
+        return """-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAyWp165X5cb3gC53VqnY8++aXyk8D14Bgt2ob1uTd1nYrE5IU
+3J3mqU9wvBsx80Xj8syWNmltH6iHH72z7j1nJi3fzbYAD7s3f3cBc8rx3XlM62xA
+Cta6aef1TU0VVxLk/VmoHqtqd85dOqlM67Wc/C+Khw0GjhmAuzyMCis/MQtj2ncx
++gt0RnkrZIG2XI3RefbOtOVI7DL8nCjup0y5b4dsZbyI/Za+3bTw0S2JNSEkrylX
+MgWM/QdOfPC4TI8ij7hEYI1x+U5nFFQCis5wRV1Da53bHqUAQaWz9ysYm6SRVgaJ
+pG+hxyWgxMyqD81B8KK2qNbv8GTQZp96fBD15QIDAQABAoIBAGHXkYjrxcz3C8sY
+1R6FaYKEtd/VGmypNFJk/Tka3Ji9tpc/M1soaVB+AqmeHFms7dqYf6/W7ueeGmXU
+5X8p3N2zEzD+5HC/5WnKCa6SO4P21OLYJxQc01l3ELaIZ4Fw8EQSNZUQeBvS538D
+lxY5lxT6kzSfozuML/jEpNKbx4quvQqF1NwtiOTJPDOHWozyUQhNcsTOYrucuV4j
+dCsY6tku47qR+8eaWkfK+EWmBisYlfSlJgP77orXd5lnhTZJkCTnjNB8SKlYXCvi
+B99u15FLjmS8QtPbq5NuW8gEFuMajNNWxdXFlMhTlK+mQ+cNlmlt4CUaLqbFHcTI
+4buIylECgYEA6re+ftA3ARSi1wac24yxJFugFIuLm4Vq2aACvFcm3+dD8RjpK51u
+V97EvAQyCz3eZlAhw5mx8w99QeHnLTtbvU8aFns3t+9XjMOi8D8PWqB7LVEPet/y
+uEYT+DaVyeIuXGWE0EYUjFp2ZWz47TMqaAtcgRRaAhhUPlsnrJA9OGsCgYEA2623
+AAVuo76zvpGmmVqldygxhRqDVaPFTFusFIpC0IqZAClNSGQxhaG2645vet54vW20
+N04TDuvZu+3ea9pOIxoFxLdtQxzFwc27G96GYs5yehHZrWhMMcvSMrYAhZJvKcuV
+sGPkkV9dhmPNBNYQAcF48+ha7s0ha6gnXz0WXu8CgYBnpMlWYATZ0k3xmzbqb57N
+CjSOvevgubIr9M8gcW92ET3cGX7kxniyDmlbCJM8iY5KKXovUA/W33EVBXa6b2qc
+FnDTmodJDkPfoYeyhHX4M8MQiKnIPVmFa1RoF1pfMiP8ostZ2Ig8TbnYIZ1tyFki
+ZlnrmZwBLtMMUd4cAfk2jwKBgQCLmLJHsywBpfe2y/ugS/gF5mbBfH2k+DMyOLb7
+PnkC6HAdqCFSNUk67+67dYHmBGWZipMQ6e42dy2fvKKwysMIj0rHHQCzux737vJN
+3XRsaWBZreozxalVo33pd7qRJuK5HoP62R+wOXfHJLhtsFHvq7Be3nKaeYQZU3vh
+i4IhxQKBgQDlJEb+0ZnDlsB1UcMHhXtsO31WrNOn9SiGnDVMc0ik0pBs/06ZIQ/4
+Zb8O2Nm8VTNZARdKHN1ll7GT6kNAr6Ae9lc2oEB/EdODZ3ARtUJF1HNxxgl+hwfN
+U+27XptJXHsIBqoIbIbx+/TVejFlv8Lp46SdtvgKPXY2pZhtn+3icQ==
+-----END RSA PRIVATE KEY-----"""
 
     def setUp(self):
         pass
@@ -22,8 +58,179 @@ class TestTransfer(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_foo(self):
-        self.assertEqual(2, 2)
+    def test_transfer_base_class(self):
+        t = Transfer(None)
+        t.connect()
+        t.disconnect()
+        msg, duration, bytes_transferred = t.transfer_file('foo')
+        self.assertEqual(msg, 'Not implemented')
+        self.assertEqual(duration, -1)
+        self.assertEqual(bytes_transferred, -1)
+
+    def test_sftptransfer_get_value_from_config(self):
+        # test with None for Config
+        t = SftpTransfer(None)
+        res = t._get_value_from_config(SftpTransfer.SECTION,
+                                       SftpTransfer.HOST)
+        self.assertEqual(res, None)
+
+        # test with no section
+        con = configparser.ConfigParser()
+        t = SftpTransfer(con)
+        res = t._get_value_from_config(SftpTransfer.SECTION,
+                                       SftpTransfer.HOST)
+        self.assertEqual(res, None)
+
+        # test with no option
+        con.add_section(SftpTransfer.SECTION)
+        t = SftpTransfer(con)
+        res = t._get_value_from_config(SftpTransfer.SECTION,
+                                       SftpTransfer.HOST)
+        self.assertEqual(res, None)
+
+        # test valid
+        # test with no section
+        con.set(SftpTransfer.SECTION,
+                SftpTransfer.HOST, 'someval')
+        t = SftpTransfer(con)
+        res = t._get_value_from_config(SftpTransfer.SECTION,
+                                       SftpTransfer.HOST)
+        self.assertEqual(res, 'someval')
+
+    def test_sftptransfer_get_private_key(self):
+        # test with no option set
+        t = SftpTransfer(None)
+        res = t._getprivatekey()
+        self.assertEqual(res, None)
+
+        # test with valid key
+        temp_dir = tempfile.mkdtemp()
+        try:
+            # test with invalid key
+            dkey = os.path.join(temp_dir, 'dummy.key')
+            con = configparser.ConfigParser()
+            con.add_section(SftpTransfer.SECTION)
+            con.set(SftpTransfer.SECTION,
+                    SftpTransfer.KEY, dkey)
+            t = SftpTransfer(con)
+
+            # test with invalid key
+            try:
+                t._getprivatekey()
+                self.fail('Expected IOError')
+            except IOError:
+                pass
+
+            # test with valid key
+            f = open(dkey, 'w')
+            f.write(self._get_dummy_private_key())
+            f.flush()
+            f.close()
+            res = t._getprivatekey()
+            self.assertEqual(res.get_name(), 'ssh-rsa')
+
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_sftptransfer_get_host(self):
+        con = configparser.ConfigParser()
+        con.add_section(SftpTransfer.SECTION)
+        con.set(SftpTransfer.SECTION,
+                SftpTransfer.HOST, 'host')
+        t = SftpTransfer(con)
+        self.assertEqual(t._gethost(), 'host')
+
+    def test_sftptransfer_getusername(self):
+        con = configparser.ConfigParser()
+        con.add_section(SftpTransfer.SECTION)
+        con.set(SftpTransfer.SECTION,
+                SftpTransfer.USER, 'user')
+        t = SftpTransfer(con)
+        self.assertEqual(t._getusername(), 'user')
+
+    def test_sftptransfer_get_missing_host_key_policy(self):
+        t = SftpTransfer(None)
+        res = t._get_missing_host_key_policy()
+        self.assertTrue('AutoAddPolicy' in str(res))
+
+    def test_sftptransfer_getport(self):
+        t = SftpTransfer(None)
+        self.assertEqual(t._getport(), 22)
+
+        con = configparser.ConfigParser()
+        con.add_section(SftpTransfer.SECTION)
+        con.set(SftpTransfer.SECTION,
+                SftpTransfer.PORT, '23')
+        t = SftpTransfer(con)
+        self.assertEqual(t._getport(), 23)
+
+    def test_sftptransfer_get_destdir(self):
+        con = configparser.ConfigParser()
+        con.add_section(SftpTransfer.SECTION)
+        con.set(SftpTransfer.SECTION,
+                SftpTransfer.DEST_DIR, 'dir')
+        t = SftpTransfer(con)
+        self.assertEqual(t._getdestdir(), 'dir')
+
+    def test_sftptransfer_getconnecttimeout(self):
+        t = SftpTransfer(None)
+        self.assertEqual(t._getconnecttimeout(), 60)
+
+        con = configparser.ConfigParser()
+        con.add_section(SftpTransfer.SECTION)
+        con.set(SftpTransfer.SECTION,
+                SftpTransfer.CON_TIMEOUT, '45')
+        t = SftpTransfer(con)
+        self.assertEqual(t._getconnecttimeout(), 45)
+
+    def test_getdestdir(self):
+        t = SftpTransfer(None)
+        res = t._getdestdir()
+        self.assertEqual(res, None)
+        con = configparser.ConfigParser()
+        con.add_section(SftpTransfer.SECTION)
+        con.set(SftpTransfer.SECTION,
+                SftpTransfer.DEST_DIR, '/foo')
+        t = SftpTransfer(con)
+        res = t._getdestdir()
+        self.assertEqual(res, '/foo')
+
+    def test_connect_invalid_connection(self):
+        con = configparser.ConfigParser()
+        con.add_section(SftpTransfer.SECTION)
+        con.set(SftpTransfer.SECTION,
+                SftpTransfer.HOST,
+                '127.0.0.1')
+        con.set(SftpTransfer.SECTION,
+                SftpTransfer.PORT,
+                '80')
+        t = SftpTransfer(con)
+        try:
+            t.connect()
+            self.fail('Expected some error')
+        except:
+            pass
+
+    def test_connect_alt_ssh_set(self):
+        t = SftpTransfer(None)
+        t.set_alternate_connection('foo')
+        t.connect()
+        self.assertEqual(t._ssh, 'foo')
+
+    def test_disconnect_without_connect_call(self):
+        t = SftpTransfer(None)
+        t.disconnect()
+        self.assertEqual(t._sftp, None)
+        self.assertEqual(t._ssh, None)
+
+    def test_disconnect_where_both_cause_exceptions(self):
+        t = SftpTransfer(None)
+        t._sftp = 'foo'
+        t._ssh ='hi'
+        t.disconnect()
+        self.assertEqual(t._sftp, None)
+        self.assertEqual(t._ssh, None)
+
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
