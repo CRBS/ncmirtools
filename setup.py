e0feb4728dd61ae7081132a8f36fb8a701df7b1e
Chris Churas <churas@gmail.com>
2017-05-18 15:34:29 -0700

    bumped release to 0.4.0rc2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='ncmirtools',
-    version='0.4.0rc',
+    version='0.4.0rc2',
     description="Set of commandline tools for NCMIR",
     long_description=readme + '\n\n' + history,
     author="Christopher Churas",
